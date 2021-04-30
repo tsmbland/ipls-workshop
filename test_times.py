6f9d5c556b2a78aa780d4e08392672a194f46475
Tom Bland <tom_bland@hotmail.co.uk>
2021-04-30 14:37:36 +0100

    Add workshop timing test. Answers UCL_RITS/ipls-workshop#5

--- test_times.py
+++ test_times.py
@@ -10,4 +10,9 @@ def test_compute_overlap_within():
 def test_workshop_morning_times():
     # replace ... with your code for the additional test here
     # the structure should be very similar to the test above!
-    ...
+    time_range_1 = time_range("2021-04-30 10:00:00", "2021-04-30 13:00:00")
+    time_range_2 = time_range("2021-04-30 10:05:00", "2021-04-30 12:55:00", 3, 600)
+    result = compute_overlap_time(time_range_1, time_range_2)
+    expected = [("2021-04-30 10:05:00", "2021-04-30 10:55:00"), ("2021-04-30 11:05:00", "2021-04-30 11:55:00"), ("2021-04-30 12:05:00", "2021-04-30 12:55:00")]
+    assert result == expected
+     
